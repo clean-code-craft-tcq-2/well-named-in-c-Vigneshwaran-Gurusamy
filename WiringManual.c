9b72477702e4bda5b26b691e85c5364b9aa411a1
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-07 17:27:32 +0530

    Update WiringManual.c

--- WiringManual.c
+++ WiringManual.c
@@ -1,5 +1,19 @@
 #include "WiringManual.h"
 
+/*Definition of Variables and Functions*/
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
