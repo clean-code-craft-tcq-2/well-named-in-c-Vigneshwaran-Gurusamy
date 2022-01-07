f11f93e2d0f212fb3fa39af8a928424b48a46572
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-07 17:34:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include "WiringManual.h"
 
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got Pair -> %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got PairNumber = %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
+
 /*Check the Wiring Pair Manual*/
 int main() {
     testNumberToPair(4, WHITE, BROWN);
