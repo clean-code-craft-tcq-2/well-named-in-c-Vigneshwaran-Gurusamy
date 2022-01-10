6f13238033bf801cdcbb6ece789d57a4a1793a4c
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-10 13:29:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,39 @@
 #include "WiringManual.h"
-
-void testNumberToPair(int pairNumber,
+    
+/*Check the Major and Minor color matches the PairNumber*/
+void testNumberToPair(int PairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
 {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got Pair -> %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
+    char colorNames[ColorName_Chars];
+    ColorPair color = GetColorFromPairNumber(PairNumber);
+    ColorPairToString(&color, colorNames);
+    printf("Colors are -> %s for the PairNumber %d\n", colorNames, PairNumber);
+    assert(color.majorColor == expectedMajor);
+    assert(color.minorColor == expectedMinor);
 }
 
+/*Check the PairNumber matches the Major and Minor color*/
 void testPairToNumber(
     enum MajorColor major,
     enum MinorColor minor,
     int expectedPairNumber)
 {
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got PairNumber = %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
+    char colorNames[ColorName_Chars];
+    ColorPair color;
+    color.majorColor = major;
+    color.minorColor = minor;
+    ColorPairToString(&color, colorNames);
+    int PairNumber = GetPairNumberFromColor(&color);
+    printf("PairNumber is %d for the Colors -> %s\n", PairNumber,colorNames);
+    assert(PairNumber == expectedPairNumber);
 }
 
-/*Check the Wiring Pair Manual*/
-int main() {
+/*Check the Wiring PairNumber and Wiring colors*/
+void main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
-    return 0;
 }
