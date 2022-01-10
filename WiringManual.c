34a2296263cb335316fffb81b58ca27626821b3a
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-10 13:29:15 +0530

    Update WiringManual.c

--- WiringManual.c
+++ WiringManual.c
@@ -1,25 +1,27 @@
 #include "WiringManual.h"
 
 /*Definition of Variables and Functions*/
-const int MAX_COLORPAIR_NAME_CHARS = 10;
-
+const int ColorName_Chars = 10;
+/*Major Colors*/
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-
+/*Minor Colors*/
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-
+/*Total MajorColors and MinorColors*/
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+/*Get the ColorNames as string*/
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s and %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
 
+/*Get the Major, Minor Colors from pairnumber*/
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
@@ -30,6 +32,7 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
     return colorPair;
 }
 
+/*Get the pairnumber from Colors*/
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
