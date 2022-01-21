fa4bcb135f099450fc26362633c80364a1ebf762
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-21 20:29:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include "WiringManual.h"
 
-/*Check the Wiring PairNumber and Wiring colors*/
+/*Test the Wiring PairNumber and Wiring colors*/
 void main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+    PrintWiringManual(); /*Print Wiring Manual*/
+    
+}
+
+/*Print WiringManual*/
+void PrintWiringManual(void){
+    int PairNumber = 1;
+    ColorPair color;
+    
+    for(PairNumber = 1; PairNumber <= MAXPAIR; PairNumber++)
+    {
+        color = GetColorFromPairNumber(PairNumber);
+         printf("\n*********************");
+         printf("\n Major Color : %s  ", MajorColorNames[color.majorColor]);
+         printf("\n Minor Color : %s  ", MinorColorNames[color.minorColor]);
+         printf("\n Color Code  : %d  ", PairNumber);
+         printf("\n*********************");
+    }
 }
