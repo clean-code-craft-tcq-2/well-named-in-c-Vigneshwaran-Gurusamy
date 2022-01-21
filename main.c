7e24fc6d80cb9a5dc07d017f632db2aeb40bced4
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-21 17:50:55 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,24 +7,4 @@ void main() {
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    PrintWiringManual();
-}
-
-/*PrintWiringManual*/
-void PrintWiringManual(void){
-    unsigned int MajorColor_index = 0, MinorColor_index = 0;
-    unsigned int colorCode = 1;
-    
-    for(MajorColor_index = 0; MajorColor_index < MAXPOSSIBLECOLORS; MajorColor_index++)
-    {
-        for(MinorColor_index = 0; MinorColor_index < MAXPOSSIBLECOLORS; MinorColor_index++)
-        {
-             printf("\n*********************");
-             printf("\n Major Color : %s  ", MajorColorNames[MajorColor_index]);
-             printf("\n Minor Color : %s  ", MinorColorNames[MinorColor_index]);
-             printf("\n Color Code  : %d  ", colorCode);
-             printf("\n*********************");
-             colorCode++;
-        }
-    }
 }
