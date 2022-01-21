0d9a015bf332a0549ef80aa95715605656bacc5c
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-21 16:45:27 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "WiringManual.h"
 
+/*Check the Wiring PairNumber and Wiring colors*/
+void main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+    PrintWiringManual();
+}
+
+/*PrintWiringManual*/
 void PrintWiringManual(void){
     unsigned int MajorColor_index = 0, MinorColor_index = 0;
     unsigned int colorCode = 1;
