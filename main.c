6223e8e36e509217f264b3ff04246f25dcbae387
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-21 16:39:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ void PrintWiringManual(void){
     unsigned int MajorColor_index = 0, MinorColor_index = 0;
     unsigned int colorCode = 1;
     
-    for(MajorColor_index = 0; MajorColor_index < MaxPossibleColorsInMajorOrMinor; MajorColor_index++)
+    for(MajorColor_index = 0; MajorColor_index < MAXPOSSIBLECOLORS; MajorColor_index++)
     {
-        for(MinorColor_index = 0; MinorColor_index < MaxPossibleColorsInMajorOrMinor; MinorColor_index++)
+        for(MinorColor_index = 0; MinorColor_index < MAXPOSSIBLECOLORS; MinorColor_index++)
         {
              printf("\n*********************");
              printf("\n Major Color : %s  ", MajorColorNames[MajorColor_index]);
