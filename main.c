9b6654e08187d973ae13095bd36b46214c9f7890
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-21 16:36:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,19 @@
 #include "WiringManual.h"
+
+void PrintWiringManual(void){
+    unsigned int MajorColor_index = 0, MinorColor_index = 0;
+    unsigned int colorCode = 1;
     
-/*Check the Major and Minor color matches the PairNumber*/
-void testNumberToPair(int PairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
-    char colorNames[ColorName_Chars];
-    ColorPair color = GetColorFromPairNumber(PairNumber);
-    GetStringFromColorPair(&color, colorNames);
-    printf("Colors are -> %s for the PairNumber %d\n", colorNames, PairNumber);
-    assert(color.majorColor == expectedMajor);
-    assert(color.minorColor == expectedMinor);
-}
-
-/*Check the PairNumber matches the Major and Minor color*/
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    char colorNames[ColorName_Chars];
-    ColorPair color;
-    color.majorColor = major;
-    color.minorColor = minor;
-    GetStringFromColorPair(&color, colorNames);
-    int PairNumber = GetPairNumberFromColor(&color);
-    printf("PairNumber is %d for the Colors -> %s\n", PairNumber,colorNames);
-    assert(PairNumber == expectedPairNumber);
-}
-
-/*Check the Wiring PairNumber and Wiring colors*/
-void main() {
-    testNumberToPair(4, WHITE, BROWN);
-    testNumberToPair(5, WHITE, SLATE);
-
-    testPairToNumber(BLACK, ORANGE, 12);
-    testPairToNumber(VIOLET, SLATE, 25);
+    for(MajorColor_index = 0; MajorColor_index < MaxPossibleColorsInMajorOrMinor; MajorColor_index++)
+    {
+        for(MinorColor_index = 0; MinorColor_index < MaxPossibleColorsInMajorOrMinor; MinorColor_index++)
+        {
+             printf("\n*********************");
+             printf("\n Major Color : %s  ", MajorColorNames[MajorColor_index]);
+             printf("\n Minor Color : %s  ", MinorColorNames[MinorColor_index]);
+             printf("\n Color Code  : %d  ", colorCode);
+             printf("\n*********************");
+             colorCode++;
+        }
+    }
 }
